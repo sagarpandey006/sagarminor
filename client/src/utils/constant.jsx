6b6f635898671d61ed308bd79a411d54b28ab227
sagarpandey006 <officialsagar006@gmail.com>
2025-06-21 22:43:04 +0530

    merge conflicts

---
+++ client/src/utils/constant.jsx
@@ -0,0 +1,12 @@
+export const API_END_POINT = "https://sagarminor.onrender.com/api/v1/user";
+
+
+
+export  const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2ZTczNWUxN2QzNjQxNjE1YTdmYzE5ZSIsImlhdCI6MTcyNjQ3MTYyMiwiZXhwIjoxNzI2NDc1MjIyfQ.pT-uEgNTP6y-Jjq77RfPIHHdpL9KnHpevDLHfWY2QJc'
+      }
+    };
+ 
